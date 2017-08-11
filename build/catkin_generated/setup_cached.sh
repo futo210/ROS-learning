08fd31eb8ffcfdf0e9c7333f7ceb526a195d1416
futo210 <is0259pp@ed.ritsumei.ac.jp>
2017-08-11 16:43:43 +0900

    first commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/futo/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/opt/ros/indigo/lib:/usr/local/cuda/lib64:/usr/local/lib"
+export PATH="/opt/ros/indigo/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/bin:/home/gpu/software/intelliJ/bin:/home/gpu/software/clion/bin"
+export PKG_CONFIG_PATH="/opt/ros/indigo/lib/pkgconfig:/usr/local/lib/pkgconfig"
+export PWD="/home/futo/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/futo/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/futo/catkin_ws/src:$ROS_PACKAGE_PATH"
